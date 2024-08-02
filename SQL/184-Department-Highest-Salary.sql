7fcadb71dc18b6f8298d5f05a3c2e1afa975534f
Hoda Salim <hoda.s.salim@gmail.com>
2024-08-02 15:48:53 +0300

    [184] [Time Beats: 46.01%] - LeetPush

---
+++ SQL/184-Department-Highest-Salary.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below
+SELECT e.name AS Employee, d.name AS Department, e.salary
+FROM Employee e
+JOIN Department d ON e.departmentId = d.id
+WHERE e.salary = (
+    SELECT MAX(salary)
+    FROM Employee
+    WHERE departmentId = e.departmentId
+);
